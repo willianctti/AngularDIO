5372dabc73be559bdc1388c370f977249158081b
willianctti <willianctti@gmail.com>
2023-08-30 16:33:27 -0300

    typescript

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Ola!');
+let meuProduto = {
+    nome: "Tenis",
+    preco: 100.00,
+    unidades: 5
+};
---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log('Ola!')
+
+type produtoLoja = {
+    nome: string,
+    preco: number,
+    unidades: number
+};
+
+let meuProduto: produtoLoja = {
+    nome: "Tenis",
+    preco: 100.00,
+    unidades: 5
+}

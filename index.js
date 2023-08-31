e565f44af3e4f2a88d9fc34c59f9801f928a6ead
willianctti <willianctti@gmail.com>
2023-08-31 16:26:00 -0300

    fnlzng TS

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 "use strict";
-console.log('Ola!');
-let meuProduto = {
-    nome: "Tenis",
-    preco: 100.00,
-    unidades: 5
-};
+// // console.log('Ola!')
+// // type produtoLoja = {
+// //     nome: string,
+// //     preco: number,
+// //     unidades: number
+// // };
+// // let meuProduto: produtoLoja = {
+// //     nome: "Tenis",
+// //     preco: 100.00,
+// //     unidades: 5
+// // }
+// //interfaces (type x interface)
+// type robot = {
+//     id: number,
+//     name: string
+// };
+// interface robot2 {
+//     id: number,
+//     name: string
+// }
+// const bot: robot = {
+//     id: 1,
+//     name: "Teste"
+// };
+// Classes
+class Character {
+    constructor(name, stregth, skill) {
+        this.name = name;
+        this.stregth = stregth;
+        this.skill = skill;
+    }
+    attack() {
+        console.log(`Attack with ${this.stregth} points`);
+    }
+}
+const p1 = new Character("WIllia", 10, 98);
--- index.ts
+++ index.ts
@@ -1,13 +1,53 @@
-console.log('Ola!')
+// // console.log('Ola!')
 
-type produtoLoja = {
-    nome: string,
-    preco: number,
-    unidades: number
-};
+// // type produtoLoja = {
+// //     nome: string,
+// //     preco: number,
+// //     unidades: number
+// // };
 
-let meuProduto: produtoLoja = {
-    nome: "Tenis",
-    preco: 100.00,
-    unidades: 5
+// // let meuProduto: produtoLoja = {
+// //     nome: "Tenis",
+// //     preco: 100.00,
+// //     unidades: 5
+// // }
+
+
+// //interfaces (type x interface)
+// type robot = {
+//     id: number,
+//     name: string
+// };
+
+// interface robot2 {
+//     id: number,
+//     name: string
+// }
+
+
+// const bot: robot = {
+//     id: 1,
+//     name: "Teste"
+// };
+
+
+
+// Classes
+
+class Character {
+    name: string;
+    stregth: number;
+    skill: number;
+
+    constructor(name: string, stregth: number, skill: number) {
+        this.name = name;
+        this.stregth = stregth;
+        this.skill = skill
+    }
+
+    attack(): void {
+        console.log(`Attack with ${this.stregth} points`);
+    }
 }
+
+const p1 = new Character("WIllia", 10, 98)

6d6402cfaeda88f2abfdf02e956faeaa88d49e56
willianctti <willianctti@gmail.com>
2023-09-04 17:23:23 -0300

    event binding, tw db, s binding

---
+++ projetoteste/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EntryDataComponent } from './app/components/entry-data/entry-data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EntryDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
